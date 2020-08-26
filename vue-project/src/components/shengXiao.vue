22d8a462bebac0a1b6be051a2d9896f1a16ae41a
chendeting <1180434904@qq.com>
2020-08-26 22:21:47 +0800

    集成websocket服务

---
+++ vue-project/src/components/shengXiao.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="shengxiao">{{shengxiao}}</div>
+</template>
+
+<script>
+  import {shengxiao} from './../utils/index'
+
+  export default {
+    name: "ShengXiao",
+    data() {
+      return {
+        shengxiao: '猪'
+      }
+    },
+    created() {
+      this.timer()
+    },
+    methods: {
+      // 这是一个获取数据
+      getData(index) {
+        this.shengxiao = shengxiao()[index].name
+      },
+      // 这是一个定时器
+      timer() {
+        return setInterval(() => {
+          let _index = Math.floor(Math.random()*12)
+           this.getData(_index)
+        }, 100)
+      }
+    },
+    destroyed() {
+      clearInterval(this.timer)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- vue-project/src/page/index.vue
+++ vue-project/src/page/index.vue
@@ -13,9 +13,9 @@
     </div><!--倒计时模块-->
     <div class="flex-column plr-3">
       <div class="content-title flex-row justify-between align-center">
-        <div class="ft_18 title-left c_red">澳门特区</div>
+        <div class="ft_18 title-left c_red">澳門六合彩</div>
         <div class="ft_18 title-right c_red flex-row align-center">
-          <div>第2020028期</div>
+          <div v-if="qs">第{{qs}}期</div>
           <div class="vertical-line"></div>
           <div class="flex-column">
             <span>下期截止時間: </span>
@@ -23,75 +23,108 @@
           </div>
         </div>
       </div>
+      <div v-if="Number(kjstatue) === 3 || Number(kjstatue) === 100" class="kaijian-bg">
+        <img src="./../assets/images/QIU.gif" alt="">
+      </div>
       <ul class="ball-content flex-row center">
         <li class="ball-item flex-column">
           <div class="flex flex-column center">
-            <img :src="formatterImg(hmData[0].hm)" class="hm-img" alt="">
-            <span class="first-text-t">{{hmData[0].sx}}</span>
+            <template v-if="hmData[0]">
+              <img v-if="hmData[0].hm" :src="formatterImg(hmData[0].hm)" class="hm-img" alt="">
+              <img v-if="!hmData[0].hm" src="./../assets/images/hm2.gif" class="hm-img" alt="">
+              <span v-if="hmData[0].sx" class="first-text-t">{{hmData[0].sx}}</span>
+              <sheng-xiao v-if="!hmData[0].sx" class="first-text-t"></sheng-xiao>
+            </template>
           </div>
         </li>
         <li class="ball-item">
           <div class="flex flex-column center">
-            <img :src="formatterImg(hmData[1].hm)" class="hm-img" alt="">
-            <span class="first-text-t">{{hmData[1].sx}}</span>
+            <template v-if="hmData[1]">
+              <img v-if="hmData[1].hm" :src="formatterImg(hmData[1].hm)" class="hm-img" alt="">
+              <img v-if="!hmData[1].hm" src="./../assets/images/hm4.gif" class="hm-img" alt="">
+              <span v-if="hmData[1].sx" class="first-text-t">{{hmData[1].sx}}</span>
+              <sheng-xiao v-if="!hmData[1].sx" class="first-text-t"></sheng-xiao>
+            </template>
           </div>
         </li>
         <li class="ball-item">
           <div class="flex flex-column center">
-            <img :src="formatterImg(hmData[2].hm)" class="hm-img" alt="">
-            <span class="first-text-t">{{hmData[2].sx}}</span>
+            <template v-if="hmData[2]">
+              <img v-if="hmData[2].hm" :src="formatterImg(hmData[2].hm)" class="hm-img" alt="">
+              <img v-if="!hmData[2].hm" src="./../assets/images/hm3.gif" class="hm-img" alt="">
+              <span v-if="hmData[2].sx" class="first-text-t">{{hmData[2].sx}}</span>
+              <sheng-xiao v-if="!hmData[2].sx" class="first-text-t"></sheng-xiao>
+            </template>
           </div>
         </li>
         <li class="ball-item">
           <div class="flex flex-column center">
-            <img :src="formatterImg(hmData[3].hm)" class="hm-img" alt="">
-            <span class="first-text-t">{{hmData[3].sx}}</span>
+            <template v-if="hmData[3]">
+              <img v-if="hmData[3].hm" :src="formatterImg(hmData[3].hm)" class="hm-img" alt="">
+              <img v-if="!hmData[3].hm" src="./../assets/images/hm4.gif" class="hm-img" alt="">
+              <span v-if="hmData[3].sx" class="first-text-t">{{hmData[3].sx}}</span>
+              <sheng-xiao v-if="!hmData[3].sx" class="first-text-t"></sheng-xiao>
+            </template>
           </div>
         </li>
         <li class="ball-item">
           <div class="flex flex-column center">
-            <img :src="formatterImg(hmData[4].hm)" class="hm-img" alt="">
-            <span class="first-text-t">{{hmData[4].sx}}</span>
+            <template v-if="hmData[4]">
+              <img v-if="hmData[4].hm" :src="formatterImg(hmData[4].hm)" class="hm-img" alt="">
+              <img v-if="!hmData[4].hm" src="./../assets/images/hm5.gif" class="hm-img" alt="">
+              <span v-if="hmData[4].sx" class="first-text-t">{{hmData[4].sx}}</span>
+              <sheng-xiao v-if="!hmData[4].sx" class="first-text-t"></sheng-xiao>
+            </template>
+
           </div>
         </li>
         <li class="ball-item">
           <div class="flex flex-column center">
-            <img :src="formatterImg(hmData[5].hm)" class="hm-img" alt="">
-            <span class="first-text-t">{{hmData[5].sx}}</span>
+            <template v-if="hmData[5]">
+              <img v-if="hmData[5].hm" :src="formatterImg(hmData[5].hm)" class="hm-img" alt="">
+              <img v-if="!hmData[5].hm" src="./../assets/images/hm6.gif" class="hm-img" alt="">
+              <span v-if="hmData[5].sx" class="first-text-t">{{hmData[5].sx}}</span>
+              <sheng-xiao v-if="!hmData[5].sx" class="first-text-t"></sheng-xiao>
+            </template>
           </div>
         </li>
         <li class="ball-add-item c_red">+</li>
         <li class="ball-item">
           <div class="flex flex-column center">
-            <img :src="formatterImg(hmData[6].hm)" class="hm-img" alt="">
-            <span class="first-text-t">{{hmData[6].sx}}</span>
+            <template v-if="hmData[6]">
+              <img v-if="hmData[6].hm" :src="formatterImg(hmData[6].hm)" class="hm-img" alt="">
+              <img v-if="!hmData[6].hm" src="./../assets/images/hm7.gif" class="hm-img" alt="">
+              <span v-if="hmData[6].sx" class="first-text-t">{{hmData[6].sx}}</span>
+              <sheng-xiao v-if="!hmData[6].sx" class="first-text-t"></sheng-xiao>
+            </template>
           </div>
         </li>
       </ul>
 
       <el-table
-        :data="tableData"
-        class="mtb-3 kaijiang-table"
-        stripe
-        style="width: 100%">
+          v-if="tableData && tableData.length > 0"
+          :data="tableData"
+          class="mtb-3 kaijiang-table"
+          stripe
+          style="width: 100%">
         <el-table-column
-          prop="date"
-          label="期號/開獎時間"
-          width="120">
+            prop="date"
+            label="期號/開獎時間"
+            width="120">
           <template slot-scope="scope">
             <div class="flex-column center">
-              <span>{{scope.row.name}}</span>
-              <span>{{scope.row.time}}</span>
+              <span>第{{scope.row.qs}}期</span>
+              <span>{{scope.row.years}}</span>
             </div>
           </template>
         </el-table-column>
         <el-table-column
-          prop="address"
-          header-align="center"
-          label="中獎號碼">
+            prop="address"
+            header-align="center"
+            label="中獎號碼">
           <template slot-scope="scope">
             <ul class="flex-row center table-lists">
-              <template v-for="(item, index) in scope.row.hmData">
+              <template v-for="(item, index) in scope.row.hm">
                 <li v-if="index !== 6 " class="ball-item flex-column">
                   <div class="flex flex-column center">
                     <img :src="formatterImg(item.hm)" class="hm-img" alt="">
@@ -100,22 +133,30 @@
                 </li>
                 <li v-if="index === 6 " class="ball-add-item c_red">+</li>
               </template>
-              <li v-if="scope.row.hmData && scope.row.hmData.length >= 7" class="ball-item flex-column">
+              <li v-if="scope.row.hm && scope.row.hm.length >= 7" class="ball-item flex-column">
                 <div class="flex flex-column center">
-                  <img :src="formatterImg(scope.row.hmData[6].hm)" class="hm-img" alt="">
-                  <span class="first-text-t">{{scope.row.hmData[6].sx}}</span>
+                  <img :src="formatterImg(scope.row.hm[6].hm)" class="hm-img" alt="">
+                  <span class="first-text-t">{{scope.row.hm[6].sx}}</span>
                 </div>
               </li>
             </ul>
           </template>
         </el-table-column>
       </el-table>
+      <el-pagination
+          v-if="total > 0"
+          class="my-pagination"
+          layout="total, prev, pager, next"
+          @current-change="currentChange"
+          :total="total">
+      </el-pagination>
     </div>
   </div>
 </template>
 
 <script>
   import carousel from '@/components/carousel'
+  import ShengXiao from "../components/shengXiao";
 
   const heartCheck = {
     timeout: 60 * 1000,
@@ -130,7 +171,7 @@
       this.timer = setTimeout(() => {
         // console.log('发送心跳,后端收到后，返回一个心跳消息')
         // onmessage拿到返回的心跳就说明连接正常
-        ws.send(JSON.stringify({heart: 1}))
+        ws.send('HeartBeat')
         this.serverTimer = setTimeout(() => {
           // 如果超过一定时间还没响应(响应后触发重置)，说明后端断开了
           ws.close()
@@ -140,11 +181,12 @@
   }
   export default {
     components: {
+      ShengXiao,
       carousel
     },
     data() {
       return {
-        wsuri: 'wss://132.232.222.50:8901/websocket', // ws wss
+        wsuri: 'ws://127.0.0.1:8901//websocket', // ws wss
         lockReconnect: false, // 连接失败不进行重连
         maxReconnect: 5, // 最大重连次数，若连接失败
         socket: null,
@@ -153,105 +195,75 @@
         minute: 0,
         second: 0,
         timeInterval: null,
-        hmData: [{"color": "#e11313", "hm": "07", "sx": "马"}, {
-          "color": "#1aad19",
-          "hm": "06",
-          "sx": "羊"
-        }, {"color": "#1aad19", "hm": "05", "sx": "猴"}, {
-          "color": "#0096ff",
-          "hm": "04",
-          "sx": "鸡"
-        }, {"color": "#0096ff", "hm": "03", "sx": "狗"}, {
-          "color": "#e11313",
-          "hm": "02",
-          "sx": "猪"
-        }, {"color": "#e11313", "hm": "01", "sx": "鼠"}],
-        tableData: [{
-          hmData: [{"color": "#e11313", "hm": "07", "sx": "马"}, {
-            "color": "#1aad19",
-            "hm": "06",
-            "sx": "羊"
-          }, {"color": "#1aad19", "hm": "05", "sx": "猴"}, {
-            "color": "#0096ff",
-            "hm": "04",
-            "sx": "鸡"
-          }, {"color": "#0096ff", "hm": "03", "sx": "狗"}, {
-            "color": "#e11313",
-            "hm": "02",
-            "sx": "猪"
-          }, {"color": "#e11313", "hm": "01", "sx": "鼠"}],
-          name: '第2020227期',
-          time: '2020-08-23'
-        }, {
-          hmData: [{"color": "#e11313", "hm": "07", "sx": "马"}, {
-            "color": "#1aad19",
-            "hm": "06",
-            "sx": "羊"
-          }, {"color": "#1aad19", "hm": "05", "sx": "猴"}, {
-            "color": "#0096ff",
-            "hm": "04",
-            "sx": "鸡"
-          }, {
-            "color": "#0096ff",
-            "hm": "04",
-            "sx": "鸡"
-          }, {"color": "#0096ff", "hm": "03", "sx": "狗"},
-            {"color": "#e11313", "hm": "01", "sx": "鼠"}],
-          name: '第2020227期',
-          time: '2020-08-23'
-        }, {
-          hmData: [{"color": "#e11313", "hm": "07", "sx": "马"}, {
-            "color": "#1aad19",
-            "hm": "06",
-            "sx": "羊"
-          }, {"color": "#1aad19", "hm": "05", "sx": "猴"}, {
-            "color": "#0096ff",
-            "hm": "04",
-            "sx": "鸡"
-          }, {"color": "#0096ff", "hm": "03", "sx": "狗"}, {
-            "color": "#e11313",
-            "hm": "02",
-            "sx": "猪"
-          }, {"color": "#e11313", "hm": "07", "sx": "马"}],
-          name: '第2020227期',
-          time: '2020-08-23'
-        }]
+        hmData: [],
+        qs: null,
+        kjstatue: 0, // 是否正在开奖
+        currentPage: 1,
+        total: 0,
+        tableData: []
       }
     },
     mounted() {
-      // this.initWebSocket();
-      this.setTimer(12000)
+      this.initWebSocket();
+      this.getDataLists()
     },
     methods: {
+      currentChange(currentPage) {
+        this.currentPage = currentPage
+        this.getDataLists()
+      },
+      getDataLists() {
+        this.$http.post('http://127.0.0.1:8901/running/historyRecord?limit=10&curPage=' + this.currentPage)
+            .then(res => {
+              let _data = res.data && res.data.result
+              if (_data) {
+                this.tableData = []
+                this.tableData = _data.records
+                this.total = _data.total
+              }
+            })
+      },
       formatterImg(hm) {
         return require(`./../assets/images/${hm}.png`)
       },
       setTimer(intDiff) {
         let self = this;
+        self.day = 0;
+        self.hour = 0;
+        self.minute = 0;
+        self.second = 0;
         window.clearInterval(self.timeInterval);
-        self.timeInterval = window.setInterval(function () {
-          if (intDiff > 0) {
-            self.day = Math.floor(intDiff / (60 * 60 * 24));
-            self.hour = Math.floor(intDiff / (60 * 60)) - (self.day * 24);
-            self.minute = Math.floor(intDiff / 60) - (self.day * 24 * 60) - (self.hour * 60);
-            self.second = Math.floor(intDiff) - (self.day * 24 * 60 * 60) - (self.hour * 60 * 60) - (self.minute * 60);
-          }
+        if (intDiff) {
+          self.timeInterval = window.setInterval(function () {
+            if (intDiff > 0) {
+              self.day = Math.floor(intDiff / (60 * 60 * 24));
+              self.hour = Math.floor(intDiff / (60 * 60)) - (self.day * 24);
+              self.minute = Math.floor(intDiff / 60) - (self.day * 24 * 60) - (self.hour * 60);
+              self.second = Math.floor(intDiff) - (self.day * 24 * 60 * 60) - (self.hour * 60 * 60) - (self.minute * 60);
+            }
 
-          if (self.minute <= 9) self.minute = '0' + self.minute;
-          if (self.second <= 9) self.second = '0' + self.second;
-          if (self.day > 0) {
-            self.hour = self.day * 24 + self.hour;
-          } else if (self.hour <= 9) {
+            if (self.minute <= 9) self.minute = '0' + self.minute;
+            if (self.second <= 9) self.second = '0' + self.second;
+            if (self.day > 0) {
+              self.hour = self.day * 24 + self.hour;
+            } else if (self.hour <= 9) {
+              self.hour = '0' + self.hour;
+            }
+            intDiff--;
+            if (intDiff < 0) {
+              window.clearInterval(self.timeInterval);
+            }
+          }, 1000);
+        } else {
+          if (self.hour <= 9) {
             self.hour = '0' + self.hour;
           }
-          intDiff--;
-          if (intDiff < 0) {
-            window.clearInterval(self.timeInterval);
-          }
-        }, 1000);
+          if (self.minute <= 9) self.minute = '0' + self.minute;
+          if (self.second <= 9) self.second = '0' + self.second;
+        }
       },
       reconnect() {
-        console.log('尝试重连')
+        // console.log('尝试重连')
         if (this.lockReconnect || this.maxReconnect <= 0) {
           return
         }
@@ -281,9 +293,7 @@
         }
       },
       websocketOnOpen() {
-        console.log('WebSocket连接成功', this.socket.readyState)
         heartCheck.start(this.socket)
-        // this.socket.send('发送数据')
         this.websocketSend()
       },
       websocketOnError(e) {
@@ -291,20 +301,39 @@
         this.reconnect()
       },
       websocketOnMessage(e) {
-        // console.log(e)
-        let data = JSON.parse(e.data)
-        console.log('得到响应', data)
-        console.log('可以渲染网页数据...')
+        console.log('eeeeee', e)
         // 消息获取成功，重置心跳
         heartCheck.start(this.socket)
+        if (e.data === 'service_response_heart') {
+          return
+        }
+        let data = e.data ? JSON.parse(JSON.stringify(e.data)) : null
+        let dataJson = JSON.parse(data)
+        console.log('websocketOnMessage', dataJson)
+        this.setTimer(dataJson.nextkjtime)
+        this.kjstatue = dataJson.kjstatue
+        this.qs = dataJson.qs
+        if (data && dataJson.hm) {
+          this.hmData = []
+          for (let i = 0; i < 7; i++) {
+            if (dataJson.hm[i]) {
+              this.hmData.push(dataJson.hm[i])
+            } else {
+              this.hmData.push({
+                color: '',
+                hm: '',
+                sx: ''
+              })
+            }
+          }
+        }
       },
       websocketClose(e) {
-        console.log('connection closed (' + e.code + ')')
         this.reconnect()
       },
       websocketSend() {
-        let data = {id: 'a1b2c3'}
-        this.socket.send(JSON.stringify(data))
+        let data = 'HeartBeat'
+        this.socket.send(data)
       }
     },
     destroyed() {
@@ -357,6 +386,10 @@
 
   .table-lists .ball-item {
     padding: 0 .1rem;
+  }
+
+  /deep/ .my-pagination .el-pager li {
+    min-width: .7rem;
   }
 
   .ball-item {
